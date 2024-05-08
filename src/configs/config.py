a8af3c1f0bc2cf132c49d628a31a14340618134e
YaMyha <115614725+YaMyha@users.noreply.github.com>
2024-05-08 17:26:20 +0300

    Fixed fetching of env variables

--- src/configs/config.py
+++ src/configs/config.py
@@ -1,24 +1,45 @@
-from dotenv import find_dotenv, load_dotenv
-from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
+from dotenv import load_dotenv
 
-DOTENV = os.path.join(os.path.dirname(__file__), ".env-render")
+load_dotenv()
 
 
-class AppSettings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+class AppSettings:
+    @property
+    def DB_HOST(self):
+        return os.environ.get("DB_HOST")
 
-    REDIS_HOST: str
-    REDIS_PORT: int
+    @property
+    def DB_PORT(self):
+        return os.environ.get("DB_PORT")
 
-    model_config = SettingsConfigDict(env_file=DOTENV, env_file_encoding="utf-8", extra='ignore')
+    @property
+    def DB_NAME(self):
+        return os.environ.get("DB_NAME")
+
+    @property
+    def DB_USER(self):
+        return os.environ.get("DB_USER")
+
+    @property
+    def DB_PASS(self):
+        return os.environ.get("DB_PASS")
+
+    @property
+    def REDIS_HOST(self):
+        return os.environ.get("REDIS_HOST")
+
+    @property
+    def REDIS_PORT(self):
+        return os.environ.get("REDIS_PORT")
 
     @property
     def DATABASE_URL_asyncpg(self):
+        print(self.DB_HOST)
+        print(self.DB_PORT)
+        print(self.DB_NAME)
+        print(self.DB_USER)
+        print(self.DB_PASS)
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     @property
@@ -27,3 +48,33 @@ class AppSettings(BaseSettings):
 
 
 settings = AppSettings()
+
+# from dotenv import find_dotenv, load_dotenv
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+# import os
+#
+# DOTENV = os.path.join(os.path.dirname(__file__), ".env-prod")
+#
+#
+# class AppSettings(BaseSettings):
+#     DB_HOST: str
+#     DB_PORT: int
+#     DB_USER: str
+#     DB_PASS: str
+#     DB_NAME: str
+#
+#     REDIS_HOST: str
+#     REDIS_PORT: int
+#
+#     model_config = SettingsConfigDict(env_file=DOTENV, env_file_encoding="utf-8", extra='ignore')
+#
+#     @property
+#     def DATABASE_URL_asyncpg(self):
+#         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+#
+#     @property
+#     def REDIS_URL(self):
+#         return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
+#
+#
+# settings = AppSettings()
